5e3e9cfbd159761284b86766ca7d8e0dae71e2f8
Mahmoud Esteteh <esteteh.mahmoud@gmail.com>
2018-04-01 22:00:12 +0300

    Part one

---
+++ Calculator/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+namespace Calculator
+
+open System
+
+type StringCalculator() =
+
+    member this.add (number : string) =
+
+        let delimiter = ','
+        let numberList = number.Split delimiter
+        if number.Length > 0 && numberList.Length < 3 then  
+            Array.map int numberList |> Array.sum
+        else
+            0
+
+     
---
+++ Calculator/UnitTest.fs
@@ -0,0 +1,26 @@
+﻿namespace Calculator
+open System
+open NUnit.Framework
+
+[<TestFixture>]
+type Test() = 
+
+    let calculator = StringCalculator()
+
+    [<TestCase("", 0)>]
+    member this.``empty string`` input output =
+        Assert.AreEqual(output, calculator.add input)
+
+    [<TestCase("4", 4)>]
+    member this.``one number`` input output =
+        Assert.AreEqual(output, calculator.add input)
+
+    [<TestCase("4,6", 10)>]
+    member this.``two numbers`` input output =
+        Assert.AreEqual(output, calculator.add input)
+
+
+
+
+
+
